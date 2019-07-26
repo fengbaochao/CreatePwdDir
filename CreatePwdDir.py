b510b60130852ac2280208bb4c82ca6dbe7b6e31
960059842 <960059842@qq.com>
2019-07-26 11:51:56 +0800

    '1、增加选择压缩文件；2、修改生成加密字典时界面卡死问题；3、修改命名方式'

--- CreatePwdDir.py
+++ CreatePwdDir.py
@@ -1,25 +1,30 @@
 """
-生成密码字典
+@生成密码字典
+@Author:Feng
+@Date:2019-07-26
 """
 
 # -*- coding: UTF-8 -*-
 import itertools as its
 from tkinter import *
+import tkinter.filedialog
 import tkinter
 import rarfile
 import os
+import threading
 
-def extractRar(filepath,pwd):
-    extractpath = os.path.join(os.path.split(filepath)[0], 'extract')
-    rf = rarfile.RarFile(filepath)
+def extractRar(filePath, pwd):
+    extractPath = os.path.join(os.path.split(filePath)[0], 'extract')
+    rf = rarfile.RarFile(filePath)
     try:
-        rf.extractall(extractpath,pwd=pwd)
+        rf.extractall(extractPath,pwd=pwd)
         return True
     except:
         return False
 
 data = {
     'end': True,
+    'filePath':'',
     'words': '',
     'minLen': 0,
     'maxLen': 0
@@ -34,31 +39,28 @@ def createControls():
     """
     def enter():
         global data
-        data={
-            'end': False,
-            'words': txtInfo.get('1.0',END),
-            'minLen': int(txtMinLen.get()),
-            'maxLen': int(txtManLen.get())+1
-        }
-        form.quit()
+        data['end']= False
+        data['words']= txtInfo.get('1.0',END)
+        data['minLen']= int(txtMinLen.get())
+        data['maxLen']= int(txtManLen.get())+1
+        #form.quit()
+        form.destroy()
 
     """
     取消事件
     """
     def cancel():
         global data
-        data = {
-            'end': True,
-            'words': '',
-            'minLen': 0,
-            'maxLen': 0
-        }
+        data['end'] = True
         form.quit()
 
+    """
+    选择发生变化时触发
+    """
     def checkChanged():
         numbers = '1234567890'
-        lowerletters = 'abcdefghijklmnopqrstuvwxyz'
-        upperletters = lowerletters.upper()
+        lowerLetters = 'abcdefghijklmnopqrstuvwxyz'
+        upperLetters = lowerLetters.upper()
         symbols = '~!@#$%^&*()_-=+{}[]\\|;:\'",.<>/?`'
         info=''
 
@@ -68,15 +70,23 @@ def createControls():
             if checkedNumber.get():
                 info+=numbers
             if checkedLower.get():
-                info += lowerletters
+                info += lowerLetters
             if checkedUpper.get():
-                info += upperletters
+                info += upperLetters
             if checkedSymbol.get():
                 info += symbols
         txtInfo['state'] = 'normal'
         txtInfo.delete(1.0, END)
         txtInfo.insert(INSERT, info)
         txtInfo['state'] =('normal' if checkedOther.get() else 'disabled')
+
+    """
+    选择文件
+    """
+    def chooseFile():
+        global data
+        filePath = tkinter.filedialog.askopenfilename(filetypes=[("rar文件","rar")])
+        data['filePath']=filePath
 
     form = tkinter.Tk()
     form.title('生成密码字典')
@@ -90,7 +100,10 @@ def createControls():
     checkedOther=BooleanVar()
 
     # 创建控件
-    Label(form,text='密码生成设置',fg='blue',font=('微软雅黑',24)).grid(row=0,columnspan=4,sticky=W)
+    rowIndex = 0
+    Label(form,text='密码生成设置',fg='blue',font=('微软雅黑',24)).grid(row=rowIndex,columnspan=4,sticky=W)
+    rowIndex = rowIndex + 1
+    Button(form, text="选择RAR文件", command=chooseFile).grid(row=rowIndex, sticky=N + W + S + E, padx=5, pady=5)
 
     list=[]
     list.append({'id':1,'text': '全部数字', 'variable': checkedNumber})
@@ -99,58 +112,81 @@ def createControls():
     list.append({'id':4,'text': '全部特殊符号', 'variable': checkedSymbol})
     list.append({'id':4,'text': '用户自定义', 'variable': checkedOther})
 
-    rowIndex=1
     for check in list:
+        rowIndex = rowIndex + 1
         Checkbutton(form, text=check['text'], variable=check['variable'],command=checkChanged).grid(row=rowIndex, columnspan=2, sticky=W)
-        rowIndex=rowIndex+1
 
     txtInfo=Text(form,width=60,height=4,state='normal')
-    txtInfo.grid(row=6,columnspan=4,sticky=N+W+S+E, padx=5, pady=5)
+    rowIndex = rowIndex + 1
+    txtInfo.grid(row=rowIndex,columnspan=4,sticky=N+W+S+E, padx=5, pady=5)
 
-    Label(form, text='最小长度').grid(row=7, column=0, sticky=W)
+    rowIndex = rowIndex + 1
+    Label(form, text='最小长度').grid(row=rowIndex, column=0, sticky=W)
     # 设置text默认值
     default_value = StringVar()
     default_value.set('1')
     txtMinLen= Entry(form,width=10,textvariable=default_value)
-    txtMinLen.grid(row=7, column=1)
-    Label(form, text='最小长度').grid(row=7, column=2, sticky=W)
+    rowIndex = rowIndex + 1
+    txtMinLen.grid(row=rowIndex, column=1)
+    Label(form, text='最小长度').grid(row=rowIndex, column=2, sticky=W)
     default_value = StringVar()
     default_value.set('4')
     txtManLen=Entry(form,width=10,textvariable=default_value)
-    txtManLen.grid(row=7, column=3)
+    txtManLen.grid(row=rowIndex, column=3)
 
-    Button(form, text="确 定", command=enter).grid(row=8,column=2,sticky=N+W+S+E, padx=5, pady=5)
-    Button(form, text="取 消", command=cancel).grid(row=8,column=3,sticky=N+W+S+E, padx=5, pady=5)
+    rowIndex = rowIndex + 1
+    Button(form, text="确 定", command=enter).grid(row=rowIndex,column=2,sticky=N+W+S+E, padx=5, pady=5)
+    Button(form, text="取 消", command=cancel).grid(row=rowIndex,column=3,sticky=N+W+S+E, padx=5, pady=5)
     #.Button(top, text="点我", command=helloCallBack)
 
     # 输出
     form.mainloop()
     return data
 
+"""
+创建密码字典并破解
+"""
+def createPwd(data):
+    print(data)
+    words = data['words'].replace('\n', '')
+    minLen = data['minLen']
+    maxLen = data['maxLen']
+    filePath = data['filePath']
+
+    dic = open('dictionary.txt', 'w')
+    index = 0
+    # 循环密码长度
+    for num in range(minLen, maxLen):
+        # 生成密码
+        keys = its.product(words, repeat=num)
+        for key in keys:
+            index = index + 1
+            pwd = "".join(key)
+            dic.write(pwd + "\n")
+            if extractRar(filePath, pwd):
+                print('次数', index, '密码', pwd, '破解成功！！！')
+                return 1
+            else:
+                print('次数', index, '密码', pwd, '密码错误！！！')
+
+    dic.close()
+
+"""
+主函数
+"""
 def main():
     # 创建控件
     data= createControls()
     if data['end']:
         return 1
-
-    words=data['words'].replace('\n','')
-    minlen=data['minLen']
-    maxlen = data['maxLen']
-
-    #dic = open('dictionary.txt', 'w')
-    # 循环密码长度
-    for num in range(minlen, maxlen):
-        # 生成密码
-        keys = its.product(words, repeat=num)
-        for key in keys:
-            pwd="".join(key)
-            if extractRar(r'E:\test\txt.rar',pwd):
-                print(pwd,'破解成功！！！')
-                return 1
-            else:
-                print(pwd,'密码错误！！！')
-            #dic.write("".join(key) + "\n")
-    #dic.close()
+    # 创建
+    t = threading.Thread(target=createPwd, args=[data,])
+    # 守护 !!!
+    t.setDaemon(True)
+    # 启动
+    t.start()
+    # 阻塞--卡死界面！
+    t.join()
 
 if __name__=='__main__':
     print('生成密码字典')
